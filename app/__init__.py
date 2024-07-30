5433cbe6ee39292f2c30d55d44ee426e1a18daa9
StarYQ <46697160+StarYQ@users.noreply.github.com>
2024-07-29 20:45:00 -0400

    Configured for vercel

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = os.environ.get('SECRET_KEY', 'default')
 
 from app import routes
